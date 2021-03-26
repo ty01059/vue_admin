c7c58f055ffd2afd9cbc8c0d6cbefe10367d38f5
Dong1414 <homar1@naver.com>
2021-03-26 13:35:10 +0900

    리스트 디자인변경

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -4,29 +4,18 @@
 
     <section class="section section-article-list px-2">
       <div class="container mx-auto">
-        <div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
+        <div class="mt-6 mx-auto" v-bind:key="article.id" v-for="article in state.articles">
           <div class="px-6 py-6 bg-white rounded-lg shadow-md">
             <div class="flex justify-between items-center">
-              <span class="font-light text-gray-600">{{ article.regDate }}</span>
               <router-link
                 :to="'list?boardId=' + article.boardId"
                 class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
               >{{ article.boardId == 1 ? "공지" : "자유" }}</router-link>
             </div>
-            <div class="mt-2">
-              <router-link
-                :to="'detail?id=' + article.id"
-                class="text-2xl text-gray-700 font-bold hover:underline"
-              >{{ article.title }}</router-link>
-              <p class="mt-2 text-gray-600">{{ article.body }}</p>
-            </div>
-            <div class="flex justify-between items-center mt-4">
-              <router-link
-                :to="'detail?id=' + article.id"
-                class="text-blue-500 hover:underline"
-              >자세히 보기</router-link>
+            <div class="mt-2 flex items-center">
+              <span class="font-light text-gray-600 mx-14">{{ article.regDate }}</span>
               <div>
-                <router-link :to="'detail?id=' + article.id" class="flex items-center">
+                <router-link :to="'detail?id=' + article.id" class="flex items-center mx-14">
                   <img
                     src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
                     alt="avatar"
@@ -35,6 +24,10 @@
                   <h1 class="text-gray-700 font-bold hover:underline">{{article.extra__writer}}</h1>
                 </router-link>
               </div>
+              <router-link
+                :to="'detail?id=' + article.id"
+                class="text-2xl text-gray-700 font-bold hover:underline mx-14"
+              >{{ article.title }}</router-link>
             </div>
           </div>
         </div>
